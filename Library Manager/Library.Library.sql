7070dc7431fe0d1527c1347b5ec7a56270afa832
Bethany Weddle <bnweddle@ksu.edu>
2019-04-17 17:26:34 -0500

    Creating Tables

---
+++ Library Manager/Library.Library.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'Libraries.Library') IS NULL
+BEGIN
+   CREATE TABLE Libraries.Library
+   (
+		LibraryID INT NOT NULL IDENTITY(1,1), 
+		[Name] NVARCHAR(256) NOT NULL UNIQUE,
+		City NVARCHAR(64) NOT NULL,
+		[State] NVARCHAR(64) NOT NULL,
+
+      CONSTRAINT [PK_Libraries_Library_LibrayID] PRIMARY KEY CLUSTERED
+      (
+         LibraryID ASC
+      )  
+   );
+END;
+
+/*Unique Key Constraits*/
+IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.key_constraints kc
+      WHERE kc.parent_object_id = OBJECT_ID(N'Libraries.Library')
+         AND kc.[name] = N'UK_Libraries_Library_Name'
+   )
+BEGIN
+   ALTER TABLE Libraries.Library
+   ADD CONSTRAINT [UK_Library_Library_Name] UNIQUE NONCLUSTERED
+	(
+		[Name] ASC
+	)
+END;
---
+++ Library Manager/Member.sql
@@ -0,0 +1,22 @@
+﻿IF OBJECT_ID(N'Libraries.Member') IS NULL
+BEGIN
+   CREATE TABLE Libraries.Member
+   (
+		MemberID INT NOT NULL IDENTITY(1,1),
+		LibraryID INT NOT NULL REFERENCES Libraries.Library(LibraryID),
+		FirstName NVARCHAR(32) NOT NULL,
+		LastName NVARCHAR(32) NOT NULL,
+		Email NVARCHAR(128) NOT NULL,
+		Phone NVARCHAR(64) NOT NULL,
+
+		UNIQUE(MemberID, Email),
+		UNIQUE(LibraryID, MemberID),
+
+       CONSTRAINT [PK_Libraries_Member_MemberID] PRIMARY KEY CLUSTERED
+       (
+	       MemberID ASC
+       )  
+   );
+END;
+
+
