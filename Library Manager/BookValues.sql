e9b58e20579a97a7337fdd54e88f9e67639164ef
Bethany Weddle <bnweddle@ksu.edu>
2019-04-18 14:11:40 -0500

    Values for Tables

---
+++ Library Manager/BookValues.sql
@@ -0,0 +1,9 @@
+﻿INSERT Libraries.Book(TitleID, LibraryID, Quantity)
+SELECT T.TitleID, L.LibraryID, Q.Quantity
+FROM Libraries.Title T
+	CROSS JOIN Libraries.[Library] L
+	CROSS APPLY
+	(
+		VALUES(T.TitleID * L.LibraryID % 5)
+	) Q (Quantity)
+	WHERE Q.Quantity > 0
