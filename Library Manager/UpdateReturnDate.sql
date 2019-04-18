f9e90e71a1476f8f9b906c6ec2b9771a6c31952e
Bethany Weddle <bnweddle@ksu.edu>
2019-04-18 15:21:36 -0500

    UpdateReturnDate

--- Library Manager/UpdateReturnDate.sql
+++ Library Manager/UpdateReturnDate.sql
@@ -1,1 +1,10 @@
-﻿
+﻿CREATE PROCEDURE Libraries.UpdateReturnDate
+	@ItemsOutID INT
+AS
+
+UPDATE Libraries.ItemsOut 
+SET ReturnedDate = GETDATE()
+WHERE ItemsOutID = @ItemsOutID
+	AND ReturnedDate IS NULL
+
+/*Updates the returned date of a book from given ID*/
