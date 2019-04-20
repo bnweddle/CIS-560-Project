e05ca15e84e7e3119e5b19aaa546332a282fbc51
knzeiler <knzeiler@ksu.edu>
2019-04-20 18:43:48 -0500

    FindBookByTitle Delegate - Partial

---
+++ Library Manager/Library Manager/DataDelegates/FindBookByTitleDataDelegate.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+using Library_Manager.Models;
+using System;
+
+namespace Library_Manager.DataDelegates
+{
+    internal class FindBookByTitleDataDelegate : DataReaderDelegate<List<Title>>
+    {
+        string TitleName { get; }
+
+        public FindBookByTitleDataDelegate(string titleName) : base("Libraries.FindBookByTitle")
+        {
+            if (string.IsNullOrWhiteSpace(titleName))
+            {
+                throw new ArgumentException("The title name cannot be null or empty.", nameof(titleName));
+
+            }
+            TitleName = titleName;
+        }
+    }
+    public virtual void PrepareCommand(SqlCommand command)
+    {
+
+    }
+}
