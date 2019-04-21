5431189a30a1f76aea7e3c3b30dd3367a62795f6
knzeiler <knzeiler@ksu.edu>
2019-04-20 20:23:34 -0500

    ReportOverDueBooks - Completed (I think)

--- Library Manager/Library Manager/Sql/Procedures/ReportOverDueBooks.sql
+++ Library Manager/Library Manager/Sql/Procedures/ReportOverDueBooks.sql
@@ -1,1 +1,14 @@
-﻿
+﻿CREATE OR ALTER PROCEDURE Libraries.OverDueBooks
+AS
+
+SELECT T.[Name] AS BookName, IT.MemberID, IT.CheckedOutDate, IT.DueBackDate
+FROM Libraries.ItemsOut IT
+	INNER JOIN Libraries.Book B ON IT.BookID = B.BookID
+	INNER JOIN Libraries.Title T ON B.TitleID = T.TitleID
+WHERE IT.DueBackDate < CAST(GETDATE() AS DATE)
+ORDER BY IT.DueBackDate
+
+--REPORT QUERY
+--Returns the name of the book, the member ID of who has it, when it was checked out, and when it 
+--was due back 
+--I was thinking this would be sorted by the most recent over due book
