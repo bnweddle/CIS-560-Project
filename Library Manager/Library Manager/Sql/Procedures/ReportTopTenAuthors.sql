89039a683cc5f8f8101a9222535128bc383a90e6
Kassidy Zeiler <knzeiler@ksu.edu>
2019-04-20 22:33:50 -0500

    Added TopTenAuthors for Nathan

--- Library Manager/Library Manager/Sql/Procedures/ReportTopTenAuthors.sql
+++ Library Manager/Library Manager/Sql/Procedures/ReportTopTenAuthors.sql
@@ -1,1 +1,10 @@
-﻿
+﻿CREATE or ALTER PROCEDURE Libraries.ReportTopTenAuthors
+as
+
+SELECT TOP(10) WITH TIES COUNT(*) NumOfCheckOuts, a.FirstName, a.LastName
+FROM Libraries.Title t
+	INNER JOIN Libraries.Author a on a.AuthorID = t.AuthorID
+GROUP BY a.FirstName, a.LastName
+ORDER BY COUNT(*) DESC, a.FirstName desc, a.LastName desc
+
+--soz ripped this from reportmostpopularbooks
