2eec45e540121680bb7cc1d1d6f6c360653d985f
bnn678 <bnn678@ksu.edu>
2019-04-21 17:25:47 -0500

    Creation of GetMemberbyEmailDataDelegate

---
+++ Library Manager/Library Manager/DataDelegates/GetMemberbyEmailDataDelegate.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+using Library_Manager.Models;
+using System.Collections.Generic;
+
+namespace Library_Manager.DataDelegates
+{
+    internal class GetMemberbyEmailDataDelegate : DataReaderDelegate<Member>
+    {
+        string Email { get; }
+
+        public GetMemberbyEmailDataDelegate(string email)
+        : base("Libraries.GetMemberbyEmail")
+        {
+            Email = email;
+        }
+
+        public override Member Translate(SqlCommand command, SqlDataReader reader)
+        {
+            return new Member(reader.GetInt32(reader.GetOrdinal("MemberID")),
+                    reader.GetInt32(reader.GetOrdinal("LibraryID")),
+                    reader.GetString(reader.GetOrdinal("FirstName")),
+                    reader.GetString(reader.GetOrdinal("LastName")),
+                    reader.GetString(reader.GetOrdinal("Email")),
+                    reader.GetString(reader.GetOrdinal("Phone")));
+        }
+    }
+}
