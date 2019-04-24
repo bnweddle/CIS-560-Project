a618262cc9502c09a58f140198a545d5690e568c
Brandon Shaver <bnn678@users.noreply.github.com>
2019-04-24 04:01:19 -0500

    Improved the Powershell script but still doesn't work

--- Library Manager/Library Manager/Sql/RebuildDatabase.ps1
+++ Library Manager/Library Manager/Sql/RebuildDatabase.ps1
@@ -1,6 +1,11 @@
+# Set-ExecutionPolicy RemoteSigned
+# Install-Module sqlserver
+# Update-Module sqlserver
+# Import-Module sqlserver
+
 Param(
    [string] $Server = "(localdb)\MSSQLLocalDb",
-   [string] $Database = "LibraryDB"
+   [string] $Database = "master"
 )
 
 # This script requires the SQL Server module for PowerShell.
@@ -19,43 +24,44 @@ Write-Host ""
 Write-Host "Rebuilding database $Database on $Server..."
 
 # Drop Tables
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\DropTables.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\DropTables.sql"
 
 # Schema
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\Schema.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\Schema.sql"
 
 # Tables
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\Library.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\Members.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\Author.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\Title.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\Book.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Tables\ItemsOut.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\Library.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\Members.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\Author.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\Title.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\Book.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Tables\ItemsOut.sql"
 
 
 # Stored Procedures
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.CreateCheckOut.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.CreateMember.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.FindBooksByTitle.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.FindBooksByAuthor.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.GetMemberByEmail.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.GetTitleByLibrary.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.ReportMostPopularBooks.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.ReportNumberOfBooks.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.ReportOverDueBooks.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.ReportTopTenAuthors.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.RetrieveCheckedOutBooksForMember.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.RetrieveLibraryByName.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Procedures\Person.UpdateReturnDate.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.CreateCheckOut.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.CreateMember.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.FindBooksByTitle.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.FindBooksByAuthor.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.GetMemberByEmail.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.GetTitleByLibrary.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.ReportMostPopularBooks.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.ReportNumberOfBooks.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.ReportOverDueBooks.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.ReportTopTenAuthors.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.RetrieveCheckedOutBooksForMember.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.RetrieveLibraryByName.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Procedures\Person.UpdateReturnDate.sql"
 
 # Data
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\TempBooks.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\LibraryValues.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\MemberValues.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\AuthorValues.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\TitleValues.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\BookValues.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Sql\Data\ItemsOutValues.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\TempBooks.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\LibraryValues.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\MemberValues.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\AuthorValues.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\TitleValues.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\BookValues.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "Data\ItemsOutValues.sql"
 
 Write-Host "Rebuild completed."
 Write-Host ""
+#>
