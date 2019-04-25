94664cfb298b0776f2fb26ce77e1382908c16667
Bethany Weddle <bnweddle@ksu.edu>
2019-04-25 09:51:21 -0500

    Search Engine Procedures not quite right

--- Library Manager/Library Manager/Sql/Procedures/FindBooksByAuthor.sql
+++ Library Manager/Library Manager/Sql/Procedures/FindBooksByAuthor.sql
@@ -7,10 +7,10 @@ SELECT T.TitleID, T.ISBN, T.[Name], A.FullName, T.PublicationYear, T.AuthorID
 FROM Libraries.Author A
 	INNER JOIN Libraries.Title T ON T.AuthorID = A.AuthorID
 	INNER JOIN Libraries.Book B ON B.TitleID = T.TitleID
-		AND B.Quantity > 0
 	INNER JOIN Libraries.ItemsOut I ON I.BookID = B.BookID
 		AND I.ReturnedDate IS NOT NULL
-	INNER JOIN Libraries.[Library] L ON L.LibraryID = B.LibraryID
-	INNER JOIN Libraries.Member M ON M.LibraryID = L.LibraryID
+		AND I.LibraryID = B.LibraryID
+		AND B.Quantity > 0
+	INNER JOIN Libraries.Member M ON M.LibraryID = B.LibraryID
 		AND M.MemberID = @MemberID
 WHERE (A.FullName LIKE '%' + @AuthorNamePattern + '%')
--- Library Manager/Library Manager/Sql/Procedures/FindBooksByTitle.sql
+++ Library Manager/Library Manager/Sql/Procedures/FindBooksByTitle.sql
@@ -2,13 +2,13 @@
 	@TitleNamePattern NVARCHAR(265),
 	@MemberID INT
 AS
-SELECT T.TitleID, T.ISBN, T.[Name], T.AuthorID, T.PublicationYear
+SELECT DISTINCT T.TitleID, T.ISBN, T.[Name], T.AuthorID, T.PublicationYear
 FROM Libraries.Title T
 	INNER JOIN Libraries.Book B ON B.TitleID = T.TitleID
-		AND B.Quantity > 0
 	INNER JOIN Libraries.ItemsOut I ON I.BookID = B.BookID
 		AND I.ReturnedDate IS NOT NULL
-	INNER JOIN Libraries.[Library] L ON L.LibraryID = B.LibraryID
-	INNER JOIN Libraries.Member M ON M.LibraryID = L.LibraryID
+		AND I.LibraryID = B.LibraryID
+		AND B.Quantity > 0
+	INNER JOIN Libraries.Member M ON M.LibraryID = B.LibraryID
 		AND M.MemberID = @MemberID
 WHERE T.[Name] LIKE '%' + @TitleNamePattern + '%'
