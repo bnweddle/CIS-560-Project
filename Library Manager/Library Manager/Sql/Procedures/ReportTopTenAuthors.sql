f0b407546c316057da8f65341c48196305749cb7
Bethany Weddle <bnweddle@ksu.edu>
2019-04-29 08:56:38 -0500

    Report Authors

--- Library Manager/Library Manager/Sql/Procedures/ReportTopTenAuthors.sql
+++ Library Manager/Library Manager/Sql/Procedures/ReportTopTenAuthors.sql
@@ -1,19 +1,18 @@
 ﻿CREATE or ALTER PROCEDURE Libraries.ReportTopTenAuthors
-@MemberID int
+	@MemberID int
 as
 
-SELECT TOP(10) WITH TIES COUNT(*) NumOfCheckOuts, a.FirstName, a.LastName, a.MiddleName,
+SELECT TOP(3) WITH TIES COUNT(*) NumOfCheckOuts, a.FirstName, a.LastName, a.MiddleName,
 	a.FullName, a.AuthorID
 FROM Libraries.Title t
 	INNER JOIN Libraries.Author a on a.AuthorID = t.AuthorID
 	INNER JOIN Libraries.Book b on b.TitleID = t.TitleID
-	INNER JOIN Libraries.[Library] l on l.LibraryID = b.LibraryID
-	INNER JOIN Libraries.Member m on m.LibraryID = l.LibraryID
-		AND @MemberID = m.MemberID
+	INNER JOIN Libraries.ItemsOut I ON I.BookID = b.BookID
+WHERE I.MemberID = @MemberID
 GROUP BY a.FirstName, a.LastName, a.MiddleName, a.FullName, a.AuthorID
 ORDER BY COUNT(*) DESC, a.FirstName desc, a.LastName desc, a.MiddleName desc,
 	a.FullName desc, a.AuthorID DESC
     
 --REPORT QUERY
---Should return the top ten authors for the specific memeber's library 
---Decides the top ten by comparing the num of times a book has been checked out 
+--Should return the top three authors for the specific member
+--Decides the top three by comparing the num of times a book has been checked out by the member
